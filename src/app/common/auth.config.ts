e94156b073f71017e833e3ac5981917cc6aae2a7
seby <sebastianavram@gmail.com>
2016-12-02 14:26:30 +0200

    added map +fixed reservation when is authenticated

--- src/app/common/auth.config.ts
+++ src/app/common/auth.config.ts
@@ -7,5 +7,5 @@ interface AuthConfiguration {
 export const myConfig: AuthConfiguration = {
   clientID: 'zAVnpSCw0ZFgIFGR6WKBmZfoHjJZiCY4',
   domain: 'stefanavram.eu.auth0.com',
-  callbackURL: 'http://localhost:4200/'
+  callbackURL: 'https://stefanavram.github.io/frontend/'
 };
